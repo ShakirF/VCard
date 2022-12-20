ae93d47aa2c047bb3f87f451d593bbc5ad6a5f30
Shakir <shakirfarajullayev@gmail.com>
2022-12-20 23:48:31 +0400

    Add project files.

---
+++ FileHelp.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VCard_project.Models;
+
+
+namespace VCard_project
+{
+    public static class FileHelp
+    {
+        const string NewLine = "\r\n";
+        const string Separator = ";";
+        const string Header = "BEGIN:VCARD\r\nVERSION:2.1";
+        const string Name = "N:";
+        const string FormattedName = "FN:";
+        const string OrganizationName = "ORG:";
+        const string TitlePrefix = "TITLE:";
+        const string PhotoPrefix = "PHOTO;ENCODING=BASE64;JPEG:";
+        const string PhonePrefix = "TEL;TYPE=";
+        const string PhoneSubPrefix = ",VOICE:";
+        const string AddressPrefix = "ADR;TYPE=";
+        const string AddressSubPrefix = ":;;";
+        const string EmailPrefix = "EMAIL:";
+        const string Footer = "END:VCARD";
+
+        public static string CreateVCard(VCard vCard)
+        {
+            StringBuilder fw = new StringBuilder();
+            fw.Append(Header);
+            fw.Append(NewLine);
+
+            //Full Name
+            if (!string.IsNullOrEmpty(vCard.FirstName) || !string.IsNullOrEmpty(vCard.LastName))
+            {
+                fw.Append(Name);
+                fw.Append(vCard.LastName);
+                fw.Append(Separator);
+                fw.Append(vCard.FirstName);
+                fw.Append(Separator);
+                fw.Append(NewLine);
+            }
+
+
+            //Phone
+            if (!string.IsNullOrEmpty(vCard.Phone))
+                fw.Append(PhonePrefix);
+            fw.Append(vCard.Phone);
+            fw.Append(NewLine);
+
+
+
+            //Email
+            if (!string.IsNullOrEmpty(vCard.Email))
+            {
+                fw.Append(EmailPrefix);
+                fw.Append(vCard.Email);
+                fw.Append(NewLine);
+            }
+
+            //Adresses
+            if (!string.IsNullOrEmpty(vCard.Country) || !string.IsNullOrEmpty(vCard.City))
+                fw.Append(AddressPrefix);
+            fw.Append(vCard.Country);
+            fw.Append(AddressSubPrefix);
+            fw.Append(vCard.City);
+            fw.Append(NewLine);
+
+            fw.Append(Footer);
+
+            return fw.ToString();
+        }
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using VCard_project;
+using VCard_project.Models;
+
+public class Proqram
+    {
+        public static void Main()
+        {
+        int input = Convert.ToInt32(Console.ReadLine());
+        HttpClient httpClient = new HttpClient();
+        var result = httpClient.GetStreamAsync($"https://randomuser.me/api?results={input}").Result;
+        StreamReader streamReader = new StreamReader(result);
+        var json = streamReader.ReadToEndAsync().Result;
+        var deserializeObject = JsonConvert.DeserializeObject<Root>(json);
+
+        var res = deserializeObject.results.ToList();
+      
+        foreach(var item in res)
+        {
+            VCard vCard = new VCard();
+            vCard.FirstName = item.name.first;
+            vCard.LastName = item.name.last;
+            vCard.Id = item.id.value;
+            vCard.Email = item.email;
+            vCard.Phone = item.phone;
+            vCard.Country = item.location.country;
+            vCard.City = item.location.city;
+            string vcardcontents = FileHelp.CreateVCard(vCard);
+            string SavePath = System.IO.Path.Combine(AppContext.BaseDirectory, $"output_{item.id.value}.vcf");
+            System.IO.File.WriteAllText(SavePath, vcardcontents);
+            Console.WriteLine("File saved at " + SavePath.Trim());
+
+        }
+        
+    }
+
+}
